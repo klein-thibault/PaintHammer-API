1a614268c64a77d294638c7f30735c98bbcc7570
Thibault Klein <klein.thibault@gmail.com>
2021-03-18 16:35:58 -0700

    Added Heroku Postgres configuration

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,13 +7,19 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "thibaultklein",
-        password: Environment.get("DATABASE_PASSWORD") ?? "painthammer",
-        database: Environment.get("DATABASE_NAME") ?? "painthammer"
-    ), as: .psql)
+    if let databaseURL = Environment.get("DATABASE_URL"),
+       var postgresConfig = PostgresConfiguration(url: databaseURL) {
+        postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
+        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    } else {
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "thibaultklein",
+            password: Environment.get("DATABASE_PASSWORD") ?? "painthammer",
+            database: Environment.get("DATABASE_NAME") ?? "painthammer"
+        ), as: .psql)
+    }
 
     app.migrations.add(CreateProject())
     app.migrations.add(CreateStep())
